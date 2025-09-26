a6f5392a6f7a084003f9a8bb6b7fcfd056c67193
embergrowth <embergrowth@localhost>
2025-09-26 09:15:45 +0900

    fix:DB、Table定義の修正

--- backend/src/main/resources/sql/create_table.sql
+++ backend/src/main/resources/sql/create_table.sql
@@ -2,7 +2,7 @@
 CREATE SCHEMA IF NOT EXISTS app AUTHORIZATION todo_db_a9qv_user;
 
 -- Todoテーブル
-CREATE TABLE IF NOT EXISTS todos (
+CREATE TABLE IF NOT EXISTS app.todos (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     priority VARCHAR(2) NOT NULL,
@@ -10,10 +10,10 @@ CREATE TABLE IF NOT EXISTS todos (
 );
 
 -- Todoテーブル
-COMMENT ON TABLE todos IS 'タスクを管理するためのToDoリストテーブル';
+COMMENT ON TABLE app.todos IS 'タスクを管理するためのToDoリストテーブル';
 
 -- Todoテーブル
-COMMENT ON COLUMN todos.id IS 'タスクのユニークID（自動採番）';
-COMMENT ON COLUMN todos.title IS 'タスクのタイトル';
-COMMENT ON COLUMN todos.priority IS 'タスクの優先度';
+COMMENT ON COLUMN app.todos.id IS 'タスクのユニークID（自動採番）';
+COMMENT ON COLUMN app.todos.title IS 'タスクのタイトル';
+COMMENT ON COLUMN app.todos.priority IS 'タスクの優先度';
 COMMENT ON COLUMN app.todos.status IS 'タスクの状態（0=未着手, 1=進行中, 2=完了）';
